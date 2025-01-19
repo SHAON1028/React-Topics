fb647e47c792440c4caf1717a266f55c3e8f31ed
khairul Islam <khairul.islam@adndiginet.com>
2025-01-19 13:36:03 +0600

    context create

---
+++ context-api/src/App.tsx
@@ -0,0 +1,35 @@
+
+import './App.css';
+import Heading from './components/heading';
+import Section from './components/section';
+
+function App() {
+ 
+
+  return (
+    <>
+      <Section>
+        <Heading level={1}>Title</Heading>
+        <Section>
+          <Heading level={2}>Heading</Heading>
+          <Heading level={2}>Heading</Heading>
+          <Heading level={2}>Heading</Heading>
+          <Section>
+            <Heading level={3}>Sub-heading</Heading>
+            <Heading level={3}>Sub-heading</Heading>
+            <Heading level={3}>Sub-heading</Heading>
+            <Section>
+              <Heading level={4}>Sub-sub-heading</Heading>
+              <Heading level={4}>Sub-sub-heading</Heading>
+              <Heading level={4}>Sub-sub-heading</Heading>
+              <Heading level={4}>Sub-sub-heading</Heading>
+              <Heading level={4}>Sub-sub-heading</Heading>
+            </Section>
+          </Section>
+        </Section>
+      </Section>
+    </>
+  );
+}
+
+export default App
